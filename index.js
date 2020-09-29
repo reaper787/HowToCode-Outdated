6a33b8d55f620ec870c74281a0cfe5c22db1ada4
reaper787 <71782130+reaper787@users.noreply.github.com>
2020-09-29 11:55:14 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+var shaarav9 = mouseX,xy, mouseY;
+ fill(204, 255, 0);
+ var sunSize = 50;
+ ellipse(20, 20, sunSize, sunSize);
+var house = [getImage("cute/RoofEast"),
+ getImage("cute/RoofWest")];
+ var i = 0;
+var sunSize = 50;
+var cre = getImage("avatars/duskpin-ultimate");
+    
+
+ellipse(shaarav9, shaarav9, 10, 10);
+if(mouseIsPressed){draw= function() {
+var sunSize = 50;
+    sunSize+=2;
+    var sunSize = 50;
+    fill(mouseX, mouseY, mouseY);
+    ellipse(mouseX, mouseY, mouseX, mouseY);
+    ellipse(20, 20, sunSize, sunSize);
+};} 
