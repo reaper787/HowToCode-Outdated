372d892aaa6ee3f97ab85763663a7466afb6de8f
reaper787 <71782130+reaper787@users.noreply.github.com>
2020-10-06 19:37:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 function intro(){
             alert("welcome to my webpage");
             }
+$(document).ready(function(){
+   $("#story-tittle").hover(function(){
+   $(this).css("color", "black");
+   },
+   function(){
+   $(this).hide();
+   });    
+});
