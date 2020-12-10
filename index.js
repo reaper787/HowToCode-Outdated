c463f40dc41ae41b70ea4c13fb31d6842cf49617
reaper787 <71782130+reaper787@users.noreply.github.com>
2020-12-10 12:47:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,3 +14,22 @@ $(document).ready(function(){
 echo ":github: Bearer GH_TOKEN" >> ~/.gem/credentials
 gem build refactored-adventure.gemspec
 gem push --key github --host https://rubygems.pkg.github.com/reaper787 refactored-adventure-1.0.0.gem
+
+
+            var dancer1;
+var dancer2;
+
+whenSetup(function () {
+  setBackgroundEffectWithPalette("fireworks", "rave");
+  makeNewDanceSpriteGroup(9, "ALIEN", "circle");
+  changeMoveEachLR("ALIEN", "rand", -1);
+  makeNewDanceSprite("MOOSE", 'dancer1', {x: 200, y: 200});
+  changeMoveLR('dancer1', "rand", -1);
+  setForegroundEffectExtended("confetti");
+  makeNewDanceSprite("CAT", 'dancer2', {x: 200, y: 100});
+});
+
+everySeconds(4, "measures", function () {
+  changeMoveLR('dancer1', "rand", -1);
+  changeMoveLR('dancer2', "rand", -1);
+});
